bf68fa03d8d3e9f7f0a80c651baf056762b8fdfe
u0-blip <chenduona@gmail.com>
2020-11-09 19:17:02 +1100

    update start shell

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 service redis-server start
-python manage.py runserver 0.0.0.0:8000
-celery -A music.celery_task.celery worker --loglevel=info --uid 1000
+python manage.py runserver 0.0.0.0:8000 & celery -A music.celery_task.celery worker --loglevel=info
