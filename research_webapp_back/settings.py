f15be9ae618a25fefc0d47db92829561ee73244b
Ubuntu <ubuntu@ip-172-31-22-54.us-east-2.compute.internal>
2021-05-09 10:20:07 +0000

    solving cors

--- research_webapp_back/settings.py
+++ research_webapp_back/settings.py
@@ -100,7 +100,8 @@ DATABASES = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://research-web.netlify.app'
 )
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
